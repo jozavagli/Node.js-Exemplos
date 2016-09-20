bc227834c9e373ab0058a7dd4714e52f8cce1e1b
jozavagli <jozavagli@gmail.com>
2016-09-20 18:57:59 -0300

    Update 02-http-POST.js

--- http/03-http-verbos-metodos/02-http-POST.js
+++ http/03-http-verbos-metodos/02-http-POST.js
@@ -1,11 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 
-
 http.createServer(function (req, res) {
 	if (req.method ==='GET'){
 		res.writeHead(200, {'Content-Type':'text/html'});
-		fs.createReadStream('./5-coletando-POST/form.html', 'UTF-8').pipe(res);
+		fs.createReadStream('./POST/form.html', 'UTF-8').pipe(res);
 		} else if (req.method === 'POST') {
 			var body = '';
 			req.on('data', function(chunk){
